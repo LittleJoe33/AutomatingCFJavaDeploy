f38a45492992c279bb26d1230ad18a5c0b669297
LittleJoe33 <joewatson94@hotmail.co.uk>
2021-06-03 09:15:55 +0100

    error handling

--- dummyScript.sh
+++ dummyScript.sh
@@ -3,7 +3,10 @@ Lines=$(cat $File)
 RPM=$1
 regex='-(([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)\-([[:digit:]]+))\.'
 
-##git.test
+if [[ -z "$1" ]] ; then
+    echo 'No argument supplied. Expected format is "vcg-iid-simulator-MAJ.MIN.PATCH-BUILD.el7.x86_64.rpm"'
+    exit 1
+fi
 
 [[ "$RPM" =~ $regex ]]
 
@@ -12,7 +15,12 @@ umajor="${BASH_REMATCH[2]}"
 uminor="${BASH_REMATCH[3]}"
 upatch="${BASH_REMATCH[4]}"
 ubuild="${BASH_REMATCH[5]}"
- 
+
+if [[ -z "${BASH_REMATCH[1]}" ]] ; then
+    echo 'Error recognising version inputted. Expected format is "vcg-iid-simulator-MAJ.MIN.PATCH-BUILD.el7.x86_64.rpm"'
+    exit 1
+fi
+
 truncate -s 0 upgrades.txt
 truncate -s 0 downgrades.txt
 truncate -s 0 noaction.txt
@@ -25,7 +33,10 @@ for Line in $Lines
         iminor="${BASH_REMATCH[3]}"
         ipatch="${BASH_REMATCH[4]}"
         ibuild="${BASH_REMATCH[5]}"
-        
+        if [[ -z "${BASH_REMATCH[1]}" ]] ; then
+        echo 'Error recognising version on manifest. Expected format is "vcg-iid-simulator-MAJ.MIN.PATCH-BUILD.el7.x86_64.rpm"'
+        exit 1
+        fi
         if [[ "$iversion" == "$uversion" ]]; then
             echo "$Line" >> noaction.txt
         elif [[ "$imajor" > "$umajor" ]]; then
@@ -45,7 +56,7 @@ for Line in $Lines
         elif [[ "$ibuild" < "$ubuild" ]]; then
             echo "$Line" >> downgrades.txt 
         else
-            echo "Error recognising version. Expected format is ""vcg-iid-simulator-xxx.xxx.xxx-xxx.el7.x86_64.rpm"""
-            exit
+            echo 'Unknown error. Probably to do with version numbers. Expected format is "vcg-iid-simulator-MAJ.MIN.PATCH-BUILD.el7.x86_64.rpm"'
+            exit 1
         fi
     done
